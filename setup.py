a32e9f984d317e124e010e7a9f6ba59789263206
Markus Cozowicz <marcozo@microsoft.com>
2021-09-17 17:47:25 +0200

    fix more build

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="cyclicbm",
-    version="0.0.5",
+    version="0.0.6",
     description="Cyclic Boosting Machines",
     long_description=long_description,
     long_description_content_type='text/markdown',
