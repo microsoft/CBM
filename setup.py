0982919b4640384c321827e3b59ece855bdb6c80
Markus Cozowicz <marcozo@microsoft.com>
2021-11-29 09:59:45 +0100

    version bump

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="cyclicbm",
-    version="0.0.8",
+    version="0.0.9",
     description="Cyclic Boosting Machines",
     long_description=long_description,
     long_description_content_type='text/markdown',
