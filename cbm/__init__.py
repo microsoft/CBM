b0a64bbaf213b0a45a122505b9479a34edcdab7d
Markus Cozowicz <marcozo@microsoft.com>
2021-12-21 19:51:44 +0100

    added sklearn unit test

--- cbm/__init__.py
+++ cbm/__init__.py
@@ -2,6 +2,7 @@
 # Licensed under the MIT License.
 
 from .CBM import CBM
+from .sklearn import DateEncoder, TemporalSplit
 from ._version import __version__
 
-__all__ = ['CBM', '__version__']
+__all__ = ['CBM', '__version__', 'DateEncoder', 'TemporalSplit']
